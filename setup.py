c86bf060abd29149b16b3eb94616d6978e15180c
Martin Hjelmare <marhje52@gmail.com>
2026-07-20 16:11:46 +0200

    Require Python 3.10 (#116)

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=["pyevilgenius"],
     zip_safe=True,
     platforms="any",
-    python_requires=">=3.9",
+    python_requires=">=3.10",
     install_requires=list(val.strip() for val in open("requirements.txt")),
     classifiers=[
         "Intended Audience :: Developers",
